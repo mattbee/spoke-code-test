1e7c9736fe314a47f1d8ce5f0b8271fbba09d35e
Matt Bee <matt@todayishould.com>
2016-08-01 19:37:14 +0100

    Make the load more button work, using Angular two way data binding (which is awesome).

--- js/main.js
+++ js/main.js
@@ -6,12 +6,22 @@ var spokeApp = angular.module('spokeApp', []);
 
 // Register `lawyerList` component, along with its associated controller and template
 spokeApp.
-controller('people', function($scope, $http) {
-  var self = this;
-  self.orderProp = 'age';
-  self.fulldesc = false;
+	controller('people', function($scope, $http) {
+	  var self = this;
 
-  $http.get('assets/json/search-results.json').then(function(response) {
-    self.people = response.data;
-  });
-});
+	  // So the short description is shown first, set full desc to false.
+	  self.fulldesc = false;
+
+	  $http.get('assets/json/search-results.json').then(function(response) {
+	    self.people = response.data.slice(0, 6);
+	    self.peoplePage2 = response.data.slice(7, response.data.length + 1);
+	  });
+
+	  self.loadMore = function(array1, array2) {
+	  	// Fake a load more by merging the second array onto the first and add items to self.people.
+	  	// As self.people populates a ng-repeat, if it changes it will redraw the required elements.
+	  	self.people = array1.concat(array2);
+
+	  }
+
+	});
